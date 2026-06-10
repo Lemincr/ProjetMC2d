150a500c32a1274647f3ecbb4047136cda754abb
Lemincr <Delsinne9@gmail.com>
2026-06-10 17:31:09 +0200

    MAJ

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ typedef struct personnage {
     int frameActuelle;
     int timerAnim;
     bool regardeADroite;
-    bool enMouvement;
+    int vx;
 } Personnage;
 
 typedef struct squelette {
@@ -78,35 +78,24 @@ void ecrisImageInversee(int x, int y, int largeur, int hauteur, const unsigned c
 int main(int argc, char **argv)
 {
 	initialiseGfx(argc, argv);
-	
 	prepareFenetreGraphique("Minecraft 2D", LargeurFenetre, HauteurFenetre);
-	
-	/* Lance la boucle qui aiguille les evenements sur la fonction gestionEvenement ci-apres,
-		qui elle-meme utilise fonctionAffichage ci-dessous */
 	lanceBoucleEvenements();
-	
 	return 0;
 }
 
-/* Fonction de trace de cercle */
 void cercle(float centreX, float centreY, float rayon)
 {
-	const int Pas = 20; // Nombre de secteurs pour tracer le cercle
+	const int Pas = 20;
 	const double PasAngulaire = 2.*M_PI/Pas;
-	int index;
-	
-	for (index = 0; index < Pas; ++index) // Pour chaque secteur
+	for (int index = 0; index < Pas; ++index)
 	{
-		const double angle = 2.*M_PI*index/Pas; // on calcule l'angle de depart du secteur
+		const double angle = 2.*M_PI*index/Pas;
 		triangle(centreX, centreY,
 				 centreX+rayon*cos(angle), centreY+rayon*sin(angle),
 				 centreX+rayon*cos(angle+PasAngulaire), centreY+rayon*sin(angle+PasAngulaire));
-			// On trace le secteur a l'aide d'un triangle => approximation d'un cercle
 	}
 }
 
-/* La fonction de gestion des evenements, appelee automatiquement par le systeme
-des qu'une evenement survient */
 void gestionEvenement(EvenementGfx evenement)
 {
 	static bool pleinEcran = false; 
@@ -118,51 +107,38 @@ void gestionEvenement(EvenementGfx evenement)
     static Camera cam = {0};
     static Background bgJeu = {NULL, 0, 0};
     static Squelette mob1;
+    static BoutonImg bJouerMenu, bQuitterMenu;
 	
 	static int vyPerso = 0;
 	static int jumps = 0;
-	static int direction = 0;
 	
 	switch (evenement)
 	{
 		case Initialisation:
-			Player.playerPos.x = largeurFenetre()/2;
-			Player.playerPos.y = hauteurFenetre()/2;
+			Player.playerPos.x = LargeurFenetre/2;
+			Player.playerPos.y = HauteurFenetre/2;
             Player.frameActuelle = 0;
             Player.timerAnim = 0;
             Player.regardeADroite = true;
-            Player.enMouvement = false;
+            Player.vx = 0;
 
 			char chemin[50];
             for (int i = 0; i < NB_SPRITES_MARCHE; i++) {
                 if (i == 0) sprintf(chemin, "images/steve.bmp");
                 else sprintf(chemin, "images/steve%d.bmp", i + 1);
-                
                 DonneesImageRGB *img = lisBMPRGB(chemin);
                 if (img != NULL) {
                     Player.sprites[i] = img->donneesRGB;
                     Player.largeurs[i] = img->largeurImage;
                     Player.hauteurs[i] = img->hauteurImage;
-                } else {
-                    Player.sprites[i] = NULL;
-                    Player.largeurs[i] = 0;
-                    Player.hauteurs[i] = 0;
                 }
             }
 
-            // Initialisation Squelette
-            mob1.pos.x = 600;
-            mob1.pos.y = 100;
-            mob1.origineX = 600;
-            mob1.range = 200;
-            mob1.vx = 2;
-            mob1.frameActuelle = 0;
-            mob1.timerAnim = 0;
-            mob1.regardeADroite = false;
+            mob1.pos.x = 600; mob1.pos.y = 100; mob1.origineX = 600; mob1.range = 200; mob1.vx = 2;
+            mob1.frameActuelle = 0; mob1.timerAnim = 0; mob1.regardeADroite = false;
             for (int i = 0; i < NB_SPRITES_SQUELETTE; i++) {
                 if (i == 0) sprintf(chemin, "images/squelette.bmp");
                 else sprintf(chemin, "images/squelette%d.bmp", i + 1);
-                
                 DonneesImageRGB *img = lisBMPRGB(chemin);
                 if (img != NULL) {
                     mob1.sprites[i] = img->donneesRGB;
@@ -171,11 +147,28 @@ void gestionEvenement(EvenementGfx evenement)
                 }
             }
 			
-			p1 = initPlateforme(16, 16, 200*COTE_PLATEFORME, 2*COTE_PLATEFORME, "images/grass.bmp");
+			p1 = initPlateforme(16, 16, 300*COTE_PLATEFORME, 2*COTE_PLATEFORME, "images/grass.bmp");
 			
-            DonneesImageRGB *pImageFondMenu = lisBMPRGB("images/dirt.bmp");
-            if (pImageFondMenu != NULL)
-                textureFondMenu = pImageFondMenu->donneesRGB;
+            DonneesImageRGB *pImageFondMenu = lisBMPRGB("images/background.bmp");
+            if (pImageFondMenu != NULL) textureFondMenu = pImageFondMenu->donneesRGB;
+            
+            DonneesImageRGB *pImgJouer = lisBMPRGB("images/jouer.bmp");
+            if (pImgJouer != NULL) {
+                bJouerMenu.image = pImgJouer->donneesRGB;
+                bJouerMenu.largeur = pImgJouer->largeurImage;
+                bJouerMenu.hauteur = pImgJouer->hauteurImage;
+                bJouerMenu.x = LargeurFenetre / 2 - bJouerMenu.largeur / 2;
+                bJouerMenu.y = HauteurFenetre / 2 + 20;
+            }
+            
+            DonneesImageRGB *pImgLeave = lisBMPRGB("images/leave.bmp");
+            if (pImgLeave != NULL) {
+                bQuitterMenu.image = pImgLeave->donneesRGB;
+                bQuitterMenu.largeur = pImgLeave->largeurImage;
+                bQuitterMenu.hauteur = pImgLeave->hauteurImage;
+                bQuitterMenu.x = LargeurFenetre / 2 - bQuitterMenu.largeur / 2;
+                bQuitterMenu.y = HauteurFenetre / 2 - 80;
+            }
 
             DonneesImageRGB *pImageBgJeu = lisBMPRGB("images/backgroundlevel1.bmp");
             if (pImageBgJeu != NULL) {
@@ -183,31 +176,27 @@ void gestionEvenement(EvenementGfx evenement)
                 bgJeu.largeur = pImageBgJeu->largeurImage;
                 bgJeu.hauteur = pImageBgJeu->hauteurImage;
             }
-
 			demandeTemporisation(20);
 			break;
 		
 		case Temporisation:
             if (etat == ETAT_JEU) {
+                // Mouvement horizontal continu (vitesse réduite de 10 à 6)
+                Player.playerPos.x += Player.vx;
+
                 // GRAVITE
                 vyPerso -= 1;
                 gereCollisionPlateforme(&Player, p1, &vyPerso, &jumps);
                 Player.playerPos.y += vyPerso;
-		
-		if (direction == 1) {
-			Player.playerPos.x += 5;
-                        Player.regardeADroite = true;
-                        Player.enMouvement = true;
-		}
-		else if (direction == -1) {
-			Player.playerPos.x -= 5;
-                        Player.regardeADroite = false;
-                        Player.enMouvement = true;
-		}
-		
-		
+
+                // GameOver : chute dans le vide
+                if (Player.playerPos.y < -100) {
+                    printf("GAME OVER - Steve est tombe dans le vide.\n");
+                    termineBoucleEvenements(); // Tue le programme
+                }
+
                 // Animation Player
-                if (Player.enMouvement) {
+                if (Player.vx != 0) {
                     Player.timerAnim++;
                     if (Player.timerAnim >= 5) {
                         Player.frameActuelle = (Player.frameActuelle + 1) % NB_SPRITES_MARCHE;
@@ -216,24 +205,17 @@ void gestionEvenement(EvenementGfx evenement)
                 } else {
                     Player.frameActuelle = 0;
                 }
-                Player.enMouvement = false;
 
                 // IA Squelette
                 mob1.pos.x += mob1.vx;
-                if (mob1.pos.x > mob1.origineX + mob1.range) {
-                    mob1.vx = -2;
-                    mob1.regardeADroite = true;
-                } else if (mob1.pos.x < mob1.origineX - mob1.range) {
-                    mob1.vx = 2;
-                    mob1.regardeADroite = false;
-                }
+                if (mob1.pos.x > mob1.origineX + mob1.range) { mob1.vx = -2; mob1.regardeADroite = true; }
+                else if (mob1.pos.x < mob1.origineX - mob1.range) { mob1.vx = 2; mob1.regardeADroite = false; }
                 mob1.timerAnim++;
                 if (mob1.timerAnim >= 5) {
                     mob1.frameActuelle = (mob1.frameActuelle + 1) % NB_SPRITES_SQUELETTE;
                     mob1.timerAnim = 0;
                 }
 
-                // Caméra
                 cam.x = Player.playerPos.x - LargeurFenetre / 2;
                 if (cam.x < 0) cam.x = 0;
             }
@@ -243,7 +225,7 @@ void gestionEvenement(EvenementGfx evenement)
 		case Affichage:
 			effaceFenetre(255, 255, 255);
             if (etat == ETAT_MENU) {
-                afficheMenu(largeurFenetre(), hauteurFenetre(), textureFondMenu);
+                afficheMenu(LargeurFenetre, HauteurFenetre, textureFondMenu, bJouerMenu, bQuitterMenu);
             } else {
                 afficheBackground(bgJeu, cam);
                 affichePlateforme(p1, cam);
@@ -258,110 +240,58 @@ void gestionEvenement(EvenementGfx evenement)
 				case 'F':
 				case 'f':
 					pleinEcran = !pleinEcran; 
-					if (pleinEcran)
-						modePleinEcran();
-					else
-						redimensionneFenetre(LargeurFenetre, HauteurFenetre);
+					if (pleinEcran) modePleinEcran();
+					else redimensionneFenetre(LargeurFenetre, HauteurFenetre);
 					break;
-
 				case 'Q':
 				case 'q':
-                    if (etat == ETAT_JEU) {
-     
-                        			if (direction == -1) {
-                        				direction = 0;
-                        			}
-                        			else {
-                        				direction = -1;
-                        			}
-                   			
-                    }
+                    if (etat == ETAT_JEU) { Player.vx = -6; Player.regardeADroite = false; }
 					break;
 				case 'D':
 				case 'd':
-					if (etat == ETAT_JEU) {
-                        			if (direction == 1) {
-                        				direction = 0;
-                        			}
-                        			else {
-                        				direction = 1;
-                        			}
-                   			}
+					if (etat == ETAT_JEU) { Player.vx = 6; Player.regardeADroite = true; }
 					break;
+                case 'S':
+                case 's':
+                    if (etat == ETAT_JEU) Player.vx = 0;
+                    break;
 				case 'Z':
 				case 'z':
-					if (etat == ETAT_JEU && jumps != 0) {
-						vyPerso = 10;
-						jumps = 0;
-					}
+					if (etat == ETAT_JEU && jumps != 0) { vyPerso = 12; jumps = 0; }
 					break;
                 case 27: // Echap
-                    if (etat == ETAT_JEU) etat = ETAT_MENU;
+                    if (etat == ETAT_JEU) { etat = ETAT_MENU; Player.vx = 0; }
                     else termineBoucleEvenements();
                     break;
 			}
 			break;
-			
-		case ClavierSpecial:
-			break;
-
 		case BoutonSouris:
-			if (etatBoutonSouris() == GaucheAppuye)
-			{
-                if (etat == ETAT_MENU) {
-                    Bouton bJouer = getBoutonJouer(largeurFenetre(), hauteurFenetre());
-                    Bouton bQuitter = getBoutonQuitter(largeurFenetre(), hauteurFenetre());
-                    
-                    if (estSurBouton(abscisseSouris(), ordonneeSouris(), bJouer)) {
-                        etat = ETAT_JEU;
-                    } else if (estSurBouton(abscisseSouris(), ordonneeSouris(), bQuitter)) {
-                        termineBoucleEvenements();
-                    }
-                }
+			if (etatBoutonSouris() == GaucheAppuye && etat == ETAT_MENU) {
+                if (estSurBoutonImg(abscisseSouris(), ordonneeSouris(), bJouerMenu)) etat = ETAT_JEU;
+                else if (estSurBoutonImg(abscisseSouris(), ordonneeSouris(), bQuitterMenu)) termineBoucleEvenements();
 			}
 			break;
-		
-		case Souris:
-			break;
-		
-		case Inactivite:
-			break;
-		
-		case Redimensionnement:
-			break;
+		default: break;
 	}
 }
 
 void afficheBackground(Background bg, Camera cam) {
-    if (bg.texture != NULL) {
-        // Fond fixe par rapport à la caméra (bloqué sur l'écran)
-        ecrisImage(0, 0, bg.largeur, bg.hauteur, bg.texture);
-    }
+    if (bg.texture != NULL) ecrisImage(0, 0, bg.largeur, bg.hauteur, bg.texture);
 }
 
 void ecrisImageInversee(int x, int y, int largeur, int hauteur, const unsigned char *donnees) {
-    if (largeur <= 0 || hauteur <= 0) return;
     unsigned char *pixels = (unsigned char*)malloc(largeur * hauteur * 4);
     if (pixels == NULL) return;
-
     for (int j = 0; j < hauteur; j++) {
         for (int i = 0; i < largeur; i++) {
             int indexSource = (j * largeur + (largeur - 1 - i)) * 3;
             int indexDest = (j * largeur + i) * 4;
-
-            unsigned char b = donnees[indexSource];
-            unsigned char v = donnees[indexSource + 1];
-            unsigned char r = donnees[indexSource + 2];
-
-            pixels[indexDest] = b;
-            pixels[indexDest + 1] = v;
-            pixels[indexDest + 2] = r;
-            
+            unsigned char b = donnees[indexSource], v = donnees[indexSource + 1], r = donnees[indexSource + 2];
+            pixels[indexDest] = b; pixels[indexDest + 1] = v; pixels[indexDest + 2] = r;
             if (r == 255 && v == 0 && b == 254) pixels[indexDest + 3] = 0;
             else pixels[indexDest + 3] = 255;
         }
     }
-    
     extern void ecrisImageARVB(int x, int y, int largeur, int hauteur, const int *donneesARVB);
     ecrisImageARVB(x, y, largeur, hauteur, (const int*)pixels);
     free(pixels);
@@ -369,36 +299,20 @@ void ecrisImageInversee(int x, int y, int largeur, int hauteur, const unsigned c
 
 void affichePersonnage(Personnage p, Camera cam) {
     unsigned char *sprite = p.sprites[p.frameActuelle];
-    int l = p.largeurs[p.frameActuelle];
-    int h = p.hauteurs[p.frameActuelle];
-    
-    int offsetCenterX = p.largeurs[0] / 2 - l / 2;
-    int drawX = (p.playerPos.x - cam.x) + offsetCenterX;
-    int drawY = p.playerPos.y;
-
-    if (sprite != NULL && l > 0 && h > 0) {
-        if (p.regardeADroite) {
-	        ecrisImageTransparente(drawX, drawY, l, h, sprite);
-        } else {
-            ecrisImageInversee(drawX, drawY, l, h, sprite);
-        }
+    int l = p.largeurs[p.frameActuelle], h = p.hauteurs[p.frameActuelle];
+    int drawX = (p.playerPos.x - cam.x) + (p.largeurs[0] / 2 - l / 2), drawY = p.playerPos.y;
+    if (sprite != NULL) {
+        if (p.regardeADroite) ecrisImageTransparente(drawX, drawY, l, h, sprite);
+        else ecrisImageInversee(drawX, drawY, l, h, sprite);
     }
 }
 
 void afficheSquelette(Squelette s, Camera cam) {
     unsigned char *sprite = s.sprites[s.frameActuelle];
-    int l = s.largeurs[s.frameActuelle];
-    int h = s.hauteurs[s.frameActuelle];
-    
-    int drawX = (s.pos.x - cam.x);
-    int drawY = s.pos.y;
-
-    if (sprite != NULL && l > 0 && h > 0) {
-        if (s.regardeADroite) {
-            ecrisImageInversee(drawX, drawY, l, h, sprite);
-        } else {
-            ecrisImageTransparente(drawX, drawY, l, h, sprite);
-        }
+    int l = s.largeurs[s.frameActuelle], h = s.hauteurs[s.frameActuelle];
+    if (sprite != NULL) {
+        if (s.regardeADroite) ecrisImageInversee(s.pos.x - cam.x, s.pos.y, l, h, sprite);
+        else ecrisImageTransparente(s.pos.x - cam.x, s.pos.y, l, h, sprite);
     }
 }
 
@@ -406,31 +320,22 @@ void affichePlateforme(Plateforme p, Camera cam) {
     if (p.texture != NULL) {
         for (int i=0; i<(p.largeur/32); i++) {
             for (int j=0; j<(p.hauteur/32); j++) {
-                int posX = (p.coinInferieurGauche.x + 32*i + 16) - cam.x;
-                int posY = (p.coinInferieurGauche.y + 32*j + 16);
-                if (posX + 16 >= 0 && posX - 16 <= LargeurFenetre) {
-                    ecrisImage(posX, posY, 32, 32, p.texture);
-                }
+                int posX = (p.coinInferieurGauche.x + 32*i + 16) - cam.x, posY = (p.coinInferieurGauche.y + 32*j + 16);
+                if (posX + 16 >= 0 && posX - 16 <= LargeurFenetre) ecrisImage(posX, posY, 32, 32, p.texture);
             }
         }	
     }
 }
 
 Plateforme initPlateforme(int x1, int y1, int larg, int haut, char *lienTexture) {
-	Plateforme p;
-	p.coinInferieurGauche.x = x1;
-	p.coinInferieurGauche.y = y1;
-	p.largeur = larg;
-	p.hauteur = haut;
-	p.texture = NULL;
-	DonneesImageRGB * pImagePlat = lisBMPRGB(lienTexture);
-    if (pImagePlat != NULL) p.texture = pImagePlat->donneesRGB;
+	Plateforme p; p.coinInferieurGauche.x = x1; p.coinInferieurGauche.y = y1; p.largeur = larg; p.hauteur = haut; p.texture = NULL;
+	DonneesImageRGB * img = lisBMPRGB(lienTexture);
+    if (img != NULL) p.texture = img->donneesRGB;
 	return p;
 }
 
 int checkCollision(Personnage perso, Plateforme plat) {
-    int l = perso.largeurs[0];
-    int h = perso.hauteurs[0];
+    int l = perso.largeurs[0], h = perso.hauteurs[0];
 	if (perso.playerPos.x + l/4 < plat.coinInferieurGauche.x) return 0;
 	if (perso.playerPos.x - l/4 > (plat.coinInferieurGauche.x + plat.largeur)) return 0;
 	if (perso.playerPos.y + h/4 < plat.coinInferieurGauche.y) return 0;
@@ -440,11 +345,7 @@ int checkCollision(Personnage perso, Plateforme plat) {
 
 void gereCollisionPlateforme(Personnage *perso, Plateforme plat, int *vy, int *jumps) {
 	if (checkCollision(*perso, plat) == 1) {
-		if ((caractereClavier() == 'z') || (caractereClavier() == 'Z')) {
-			if (*vy >= 0) return;
-		}
-		*vy = 0;
-		perso->playerPos.y = plat.coinInferieurGauche.y + plat.hauteur + perso->hauteurs[0]/4 + 1;
-		*jumps = 1;
+		if ((caractereClavier() == 'z') || (caractereClavier() == 'Z')) { if (*vy >= 0) return; }
+		*vy = 0; perso->playerPos.y = plat.coinInferieurGauche.y + plat.hauteur + perso->hauteurs[0]/4 + 1; *jumps = 1;
 	}
 }
--- menu.c
+++ menu.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 void ecrisImageTransparente(int x, int y, int largeur, int hauteur, const unsigned char *donnees) {
+    if (donnees == NULL) return;
     unsigned char *pixels = (unsigned char*)malloc(largeur * hauteur * 4);
     if (pixels == NULL) return;
 
     for (int i = 0; i < largeur * hauteur; ++i) {
-        // donnees est en BVR (Bleu, Vert, Rouge) d'après GfxLib.c
         unsigned char b = donnees[i * 3];
         unsigned char v = donnees[i * 3 + 1];
         unsigned char r = donnees[i * 3 + 2];
@@ -16,27 +16,14 @@ void ecrisImageTransparente(int x, int y, int largeur, int hauteur, const unsign
         pixels[i * 4 + 1] = v;
         pixels[i * 4 + 2] = r;
 
-        // Couleur de transparence demandée : 255 0 254 (R V B)
-        // Attention : l'ordre dans le tableau 'donnees' est BVR d'après GfxLib.c
         if (r == 255 && v == 0 && b == 254) {
-            pixels[i * 4 + 3] = 0; // Transparent
+            pixels[i * 4 + 3] = 0;
         } else {
-            pixels[i * 4 + 3] = 255; // Opaque
+            pixels[i * 4 + 3] = 255;
         }
     }
     
-    // On utilise ecrisImageARVB de GfxLib pour envoyer les données avec alpha
-    // Sous Windows, ecrisImageARVB n'est pas dans GfxLib.h mais elle existe dans GfxLib.c
-    // Si on est sous Windows, on peut avoir besoin de la déclarer ou d'utiliser une alternative
-    // Mais d'après GfxLib.h, ecrisImageARVB est derrière un #ifndef _WIN32.
-    // Vérifions comment faire sous Windows.
 #ifdef _WIN32
-    // Sous Windows, on peut essayer d'utiliser glDrawPixels directement
-    // car GfxLib.c inclut windows.h et GL/gl.h (via GLUT probablement)
-    // Mais menu.c n'inclut pas forcément les headers OpenGL.
-    // Utilisons une approche compatible si possible.
-    // En fait, ecrisImageARVB est définie dans GfxLib.c même sous Windows, 
-    // elle n'est juste pas exportée dans le .h
     extern void ecrisImageARVB(int x, int y, int largeur, int hauteur, const int *donneesARVB);
     ecrisImageARVB(x, y, largeur, hauteur, (const int*)pixels);
 #else
@@ -46,53 +33,23 @@ void ecrisImageTransparente(int x, int y, int largeur, int hauteur, const unsign
     free(pixels);
 }
 
-Bouton getBoutonJouer(int largeurFenetre, int hauteurFenetre) {
-    Bouton b = {largeurFenetre/2 - 100, hauteurFenetre/2 + 20, 200, 40, "JOUER"};
-    return b;
-}
-
-Bouton getBoutonQuitter(int largeurFenetre, int hauteurFenetre) {
-    Bouton b = {largeurFenetre/2 - 100, hauteurFenetre/2 - 40, 200, 40, "QUITTER"};
-    return b;
-}
-
-void afficheMenu(int largeur, int hauteur, unsigned char *textureFond) {
-    if (textureFond != NULL) {
-        for (int i = 0; i < largeur; i += 32) {
-            for (int j = 0; j < hauteur; j += 32) {
-                ecrisImage(i, j, 32, 32, textureFond);
-            }
-        }
+void afficheMenu(int largeur, int hauteur, unsigned char *fond, BoutonImg bJouer, BoutonImg bQuitter) {
+    if (fond != NULL) {
+        ecrisImage(0, 0, largeur, hauteur, fond);
     } else {
         effaceFenetre(50, 50, 50);
     }
 
-    // Titre Minecraft-ish
-    couleurCourante(0, 0, 0);
-    afficheChaine("MINECRAFT 2D", 42, largeur/2 - 152, hauteur - 98);
-    couleurCourante(255, 255, 255);
-    afficheChaine("MINECRAFT 2D", 40, largeur/2 - 150, hauteur - 100);
-
-    Bouton bJouer = getBoutonJouer(largeur, hauteur);
-    Bouton bQuitter = getBoutonQuitter(largeur, hauteur);
-
-    // Dessin Bouton Jouer
-    couleurCourante(0, 0, 0); // Bordure
-    rectangle(bJouer.x - 2, bJouer.y - 2, bJouer.x + bJouer.largeur + 2, bJouer.y + bJouer.hauteur + 2);
-    couleurCourante(120, 120, 120); // Fond gris
-    rectangle(bJouer.x, bJouer.y, bJouer.x + bJouer.largeur, bJouer.y + bJouer.hauteur);
-    couleurCourante(255, 255, 255);
-    afficheChaine(bJouer.texte, 20, bJouer.x + 60, bJouer.y + 10);
-
-    // Dessin Bouton Quitter
-    couleurCourante(0, 0, 0); // Bordure
-    rectangle(bQuitter.x - 2, bQuitter.y - 2, bQuitter.x + bQuitter.largeur + 2, bQuitter.y + bQuitter.hauteur + 2);
-    couleurCourante(120, 120, 120); // Fond gris
-    rectangle(bQuitter.x, bQuitter.y, bQuitter.x + bQuitter.largeur, bQuitter.y + bQuitter.hauteur);
-    couleurCourante(255, 255, 255);
-    afficheChaine(bQuitter.texte, 20, bQuitter.x + 50, bQuitter.y + 10);
+    // Affichage des boutons images
+    if (bJouer.image != NULL) {
+        ecrisImageTransparente(bJouer.x, bJouer.y, bJouer.largeur, bJouer.hauteur, bJouer.image);
+    }
+    
+    if (bQuitter.image != NULL) {
+        ecrisImageTransparente(bQuitter.x, bQuitter.y, bQuitter.largeur, bQuitter.hauteur, bQuitter.image);
+    }
 }
 
-bool estSurBouton(int x, int y, Bouton b) {
+bool estSurBoutonImg(int x, int y, BoutonImg b) {
     return (x >= b.x && x <= b.x + b.largeur && y >= b.y && y <= b.y + b.hauteur);
 }
