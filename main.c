507c8865ad5cb337613a4f1053a5e9fe1340179f
Emirhan-Ozkan <oemirmete@gmail.com>
2026-06-15 09:49:05 +0200

    nouveaux mondes

--- main.c
+++ main.c
@@ -128,7 +128,15 @@ void gestionEvenement(EvenementGfx evenement) {
                 if (checkCollisionEcran(check, nv1)) Player.vx = 0;
                 Player.playerPos.x += Player.vx;
                 vyPerso -= 1;
-                gereCollisionEcran(&Player, nv1, &vyPerso, &jumps, &coyoteFrame);
+                
+                switch (niveauActuel) {
+                    case 1:
+                        gereCollisionEcran(&Player, nv1, &vyPerso, &jumps, &coyoteFrame);
+                        break;
+                    case 2:
+                        gereCollisionEcran(&Player, nv2, &vyPerso, &jumps, &coyoteFrame);
+                        break;
+                }
                 Player.playerPos.y += vyPerso;
 
                 if (Player.playerPos.y < -100) { vie--; Player.playerPos.x = LargeurFenetre/6.8; Player.playerPos.y = HauteurFenetre/2; vyPerso = 0; Player.vx = 0; }
